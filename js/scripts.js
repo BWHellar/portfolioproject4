6645722bf2cf0e13b9f30287e78c2cb6c1f72904
Brendan Hellar <bwhellar@Brendans-Air.hsd1.wa.comcast.net>
2019-04-12 15:45:15 -0700

    vastly pair down the logic in an attempt to convey the basic requirements before the due time

--- js/scripts.js
+++ js/scripts.js
@@ -1,94 +1,35 @@
 
-function PizzaList()  {
-  this.pizza = [];
-  this.pizzaId = 0;
+function PizzaList(sizing, meats, veggies, amount)  {
+  this.sizing = sizing;
+  this.amount = amount;
+  this.meats = meats;
+  this.veggies = veggies;
 };
 
-PizzaList.prototype.addPizza = function(pizza)  {
-  pizza.id = this.givePizzaId();
-  this.pizza.push(pizza);
-}
-PizzaList.prototype.givePizzaId = function() {
-  this.pizzaId += 1;
-  return this.pizzaId;
-}
 
-PizzaList.prototype.findPizza = function(id){
-  for(i = 0; i < this.pizza.length; i++){
-    if(this.pizza[i]) {
-      if(this.pizza[i].id == id){
-        return this.pizza[i];
-      }
-    }
-  };
-  return false;
-}
-
-function compilePizza(thisName, thisSize, thisMeat, thisVeggie) {
-  this.thisName = thisName,
-  this.thisSize = thisSize,
-  this.thisMeat = thisMeat,
-  this.thisVeggie = thisVeggie
-}
-
-var pizzaList = new PizzaList();
-
-function attachPizzaEars()  {
-  $("ol#listOfPizza").on("click", "li", function(){
-    showPizza(this.id);
-  });
+PizzaList.prototype.priceCalculate = function(){
+  var price = 0;
+  if(this.sizing === "Small"){
+    price += 10;
+  }else if (this.sizing === "Medium") {
+    price += 15;
+  }else {
+    price = 20
+  }
+  totalCost = price;
+  console.log(totalCost);
+  return totalCost * this.amount;
 };
-
-function showPizza(pizzaId) {
-  var pizza = pizzaList.findPizza(pizzaId);
-  $("#showPizza").show();
-  $("#thisName").html(pizza.thisName);
-  $("#thisSize").html(pizza.thisSize);
-  $("#thisMeat").html(pizza.thisMeat);
-  $("#thisVeggie").html(pizza.thisVeggie);
-  console.log(this.pizza);
-}
-
-function displayChosenPizza(pizzaToDisplay) {
-  var pizzaList = $("ol#listOfPizza");
-  var htmlForPizza = "";
-  pizzaToDisplay.pizza.forEach(function(pizza){
-    htmlForPizza += "<li id=" + pizza.id + ">" + pizza.thisName + "</li";
-  })
-  pizzaList.html(htmlForPizza);
-}
 
 $(document).ready(function(){
-  attachPizzaEars();
   $("form.orderForm").submit(function(event){
     event.preventDefault();
+    var totalCost = 0;
     var inputThisName = $("input#inputNewName").val();
     var inputSize = $("#sizing").find(":selected").text();
     var inputVeggie = $("#veggieList").find(":selected").text();
     var inputMeat = $("#meatList").find(":selected").text();
-    var freshPizza = new compilePizza(inputThisName, inputSize, inputMeat, inputVeggie);
-    pizzaList.addPizza(freshPizza);
-    displayChosenPizza(pizzaList);
-    console.log(freshPizza);
+    var freshPizza = new PizzaList(inputThisName, inputSize, inputMeat, inputVeggie);
+    $("#thisCost").html("$" + freshPizza.priceCalculate());
   })
-})
-
-
-
-
-
-// Similar logic might be used but needs tweaking
-
-// Pizza.prototype.priceCalculate = function(){
-//   var pizzaMath = 15 + (this.veggieMain.length * 2) + (this.meatMain.length * 3)
-//   if (this.sizing == "small"){
-//     return pizzaMath * this.amount;
-//     console.log(pizzMath * this.amount);
-//   }else if (this.sizing == "medium"){
-//     return pizzaMath * this.amount;
-//   }else if (this.sizing == "large"){
-//     return pizzaMath * this.amount;
-//   }else if (this.sizing == "xLarge"){
-//     return pizzaMath * this.amount;
-//   }
-// }
+});
